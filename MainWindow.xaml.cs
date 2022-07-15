37031c08eb16853d2527ec73bbe634a44f01520c
The Ghost <lambor590@gmail.com>
2022-07-15 02:04:42 +0200

    Agregar archivos de proyecto.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Squirrel;
+
+namespace OCM_Installer_V2
+{
+    public partial class MainWindow : Window
+    {
+        UpdateManager manager;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            UpdateHandler.HandleInstallEvents();
+
+            manager = new GithubUpdateManager(@"");
+
+            CurrentVersion.Content = manager.CurrentlyInstalledVersion().ToString();
+        }
+    }
+}
---
+++ UpdateHandler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Squirrel;
+
+namespace OCM_Installer_V2
+{
+    class UpdateHandler
+    {
+        public static void HandleInstallEvents()
+        {
+            SquirrelAwareApp.HandleEvents(onInitialInstall: OnAppInstall, onAppUpdate: OnAppUpdate, onAppUninstall: OnAppUninstall, onEveryRun: OnAppRun);
+
+            static void OnAppInstall(SemanticVersion version, IAppTools tools)
+            {
+                tools.CreateShortcutForThisExe(ShortcutLocation.StartMenu | ShortcutLocation.Desktop);
+            }
+
+            static void OnAppUninstall(SemanticVersion version, IAppTools tools)
+            {
+                tools.RemoveShortcutForThisExe(ShortcutLocation.StartMenu | ShortcutLocation.Desktop);
+            }
+
+            static void OnAppUpdate(SemanticVersion version, IAppTools tools)
+            {
+                MessageBox.Show($"Se ha actualizado el instalador a la versión {tools.CurrentlyInstalledVersion()} satisfactoriamente");
+            }
+
+            static void OnAppRun(SemanticVersion version, IAppTools tools, bool firstRun)
+            {
+                tools.SetProcessAppUserModelId();
+
+                if (firstRun) MessageBox.Show("¡Bienvenido al instalador de mods de Otako Craft! [MENSAJE TEMPORAL, PONER QUE DEBE HACER]");
+            }
+        }
+    }
+}
